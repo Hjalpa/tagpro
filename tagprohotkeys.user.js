76570c38aa533d2c89538526bc113c4c2f50dc24
Hjalpa <76667264+Hjalpa@users.noreply.github.com>
2024-09-12 11:29:27 -0400

    Create tagprohotkeys.user.js

---
+++ tagprohotkeys.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name          Map Keys to Switch Teams and Toggle Music
+// @description   Switches teams and turns music and sounds on at the push of a button
+// @version       0.12
+// @include      https://tagpro*.koalabeast.com/game
+// @include      https://tagpro*.koalabeast.com/game?*
+// @author        ballparts, Hjalpa
+// ==/UserScript==
+
+//////// Define KeyCodes Here /////////
+
+// I used this site to get key codes: https://keycode.info
+
+// KeyCode for switch teams
+var switchTeamsKeyCode = 219; // [
+
+// KeyCode for music toggle
+var musicToggleKeyCode = 221; // ]
+
+// KeyCode for sounds toggle
+var soundToggleKeyCode = 220; // \
+
+// KeyCode for chat toggle
+var chatToggleKeyCode = 189; // \
+
+///////////////////////////////////////
+
+/* globals tagpro, PIXI, jQuery, $, tinycolor, tpul */
+
+function waitForInitialized(fn) {
+    if (!tagpro) {
+        setTimeout(function() {
+            waitForInitialized(fn);
+        }, 10);
+    } else {
+        fn();
+    }
+}
+
+waitForInitialized(function() {
+    tagpro.ready(function() {
+        document.onkeydown = function(e) {
+            if($('#chat').css('display') !== "block") {
+                if(e.keyCode == switchTeamsKeyCode) {
+                    $('#switchButton').click();
+                    return;
+                }
+
+                if(e.keyCode == musicToggleKeyCode) {
+                    $('#soundMusic').click();
+                    return;
+                }
+                if(e.keyCode == soundToggleKeyCode) {
+                    $('#soundEffects').click();
+                    return;
+                }
+                if(e.keyCode == chatToggleKeyCode) {
+                    $('#chatHistory').style.display.none();
+                    return;
+                }
+            }
+        }
+    });
+});
+
+//        function hideChat() {
+//          document.getElementById("chatHistory").style.display = "none";
